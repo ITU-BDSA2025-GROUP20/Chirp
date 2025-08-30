bbf7a371e5de81a51a92f695be21de067f7b1b1e
Kian <kiaf@itu.dk>
2025-08-30 22:41:39 +0200

    Can now Cheep Co-Authored by: oskk@itu.dk gnil@itu.dk vlmi@itu.dk pafr@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 using Microsoft.VisualBasic.FileIO;
-
+using System;
 var path = "chirp_cli_db.csv";
 List<string> Authors = new List<string>();
 List<string> Messages = new List<string>();
@@ -16,20 +16,37 @@ using (TextFieldParser csvParser = new TextFieldParser(path))
     csvParser.ReadLine();
     while (!csvParser.EndOfData)
     {
+        // Read current line fields, pointer moves to the next line.
         string[] fields = csvParser.ReadFields();
         Authors.Add(fields[0]);
         Messages.Add(fields[1]);
         Timestamps.Add(fields[2]);
     }
-} // Cheers to: https://stackoverflow.com/questions/5282999/reading-csv-file-and-storing-values-into-an-array
+}
 
-
-for (int i = 0; i < Authors.Count; i++)
+if (args[0] == "read")
 {
-    DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-    dateTime = dateTime.AddSeconds(Convert.ToDouble(Timestamps[i])).ToLocalTime();
-    string[] switchedDayMonth = dateTime.ToString().Split('/');
-    switchedDayMonth[2] = switchedDayMonth[2].TrimStart('2');
-    switchedDayMonth[2] = switchedDayMonth[2].TrimStart('0');
-    Console.WriteLine("" + Authors[i] + " @ " + switchedDayMonth[1] + "/" + switchedDayMonth[0] + "/" + switchedDayMonth[2] + ": " + Messages[i]);
+
+
+    for (int i = 0; i < Authors.Count; i++)
+    {
+        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(Convert.ToDouble(Timestamps[i])).ToLocalTime();
+        string[] switchedDayMonth = dateTime.ToString().Split('/');
+        switchedDayMonth[2] = switchedDayMonth[2].TrimStart('2');
+        switchedDayMonth[2] = switchedDayMonth[2].TrimStart('0');
+        Console.WriteLine("" + Authors[i] + " @ " + switchedDayMonth[1] + "/" + switchedDayMonth[0] + "/" + switchedDayMonth[2] + ": " + Messages[i]);
+    }
+}
+else if (args[0] == "cheep")
+{
+    string csv = "";
+    string line = "";
+    for (int i = 1; i < args.Length; i++)
+    {
+        line += args[i];
+    }
+    
+    csv += Environment.UserName + ",\"" + line + "\"," + DateTimeOffset.UtcNow.ToUnixTimeSeconds() + Environment.NewLine;
+    File.AppendAllText(path, csv);
 }
