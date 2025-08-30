2097d8b97cca522291f36785fef303d99ef99f6a
Kian <kiaf@itu.dk>
2025-08-30 22:40:14 +0200

    Can now read data from .csv file. Co-Authored by: oskk@itu.dk gnil@itu.dk vlmi@itu.dk pafr@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,7 +1,35 @@
-﻿List<string> cheeps = new() { "Hello, BDSA students!", "Welcome to the course!", "I hope you had a good summer." };
+﻿
+using Microsoft.VisualBasic.FileIO;
 
-foreach (var cheep in cheeps)
+var path = "chirp_cli_db.csv";
+List<string> Authors = new List<string>();
+List<string> Messages = new List<string>();
+List<string> Timestamps = new List<string>();
+
+using (TextFieldParser csvParser = new TextFieldParser(path))
 {
-    Console.WriteLine(cheep);
-    Thread.Sleep(1000);
+    csvParser.CommentTokens = new string[] { "#" };
+    csvParser.SetDelimiters(new string[] { "," });
+    csvParser.HasFieldsEnclosedInQuotes = true;
+
+    // Skip the row with the column names
+    csvParser.ReadLine();
+    while (!csvParser.EndOfData)
+    {
+        string[] fields = csvParser.ReadFields();
+        Authors.Add(fields[0]);
+        Messages.Add(fields[1]);
+        Timestamps.Add(fields[2]);
+    }
+} // Cheers to: https://stackoverflow.com/questions/5282999/reading-csv-file-and-storing-values-into-an-array
+
+
+for (int i = 0; i < Authors.Count; i++)
+{
+    DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+    dateTime = dateTime.AddSeconds(Convert.ToDouble(Timestamps[i])).ToLocalTime();
+    string[] switchedDayMonth = dateTime.ToString().Split('/');
+    switchedDayMonth[2] = switchedDayMonth[2].TrimStart('2');
+    switchedDayMonth[2] = switchedDayMonth[2].TrimStart('0');
+    Console.WriteLine("" + Authors[i] + " @ " + switchedDayMonth[1] + "/" + switchedDayMonth[0] + "/" + switchedDayMonth[2] + ": " + Messages[i]);
 }
