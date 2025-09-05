5e3f58e81a7a5f955f08c88f5f3c9c1d4a7aed17
Kian <kiaf@itu.dk>
2025-09-05 10:03:48 +0200

    csv data can now be stored in a record file, which will next commit be used to store all of the data in the csv file.
    Co-authored-by: PipFuglen <PipFuglen@users.noreply.github.com>
    Co-authored-by: Patrick Fransgaard <pafr@itu.dk>

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,52 +1,58 @@
 ﻿
 using Microsoft.VisualBasic.FileIO;
 using System;
-var path = "chirp_cli_db.csv";
-List<string> Authors = new List<string>();
-List<string> Messages = new List<string>();
-List<string> Timestamps = new List<string>();
 
-using (TextFieldParser csvParser = new TextFieldParser(path))
-{
-    csvParser.CommentTokens = new string[] { "#" };
-    csvParser.SetDelimiters(new string[] { "," });
-    csvParser.HasFieldsEnclosedInQuotes = true;
 
-    // Skip the row with the column names
-    csvParser.ReadLine();
-    while (!csvParser.EndOfData)
+    var path = "chirp_cli_db.csv";
+    List<string> Authors = new List<string>();
+    List<string> Messages = new List<string>();
+    List<string> Timestamps = new List<string>();
+
+
+    using (TextFieldParser csvParser = new TextFieldParser(path))
     {
-        // Read current line fields, pointer moves to the next line.
-        string[] fields = csvParser.ReadFields();
-        Authors.Add(fields[0]);
-        Messages.Add(fields[1]);
-        Timestamps.Add(fields[2]);
+        csvParser.CommentTokens = new string[] { "#" };
+        csvParser.SetDelimiters(new string[] { "," });
+        csvParser.HasFieldsEnclosedInQuotes = true;
+
+        // Skip the row with the column names
+        csvParser.ReadLine();
+        while (!csvParser.EndOfData)
+        {
+            // Read current line fields, pointer moves to the next line.
+            string[] fields = csvParser.ReadFields();
+            Authors.Add(fields[0]);
+            Messages.Add(fields[1]);
+            Timestamps.Add(fields[2]);
+        }
     }
-}
 
-if (args[0].ToLower() == "read")
-{
-
-
-    for (int i = 0; i < Authors.Count; i++)
+    if (args[0].ToLower() == "read")
     {
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(Convert.ToDouble(Timestamps[i])).ToLocalTime();
-        string[] switchedDayMonth = dateTime.ToString().Split('/');
-        switchedDayMonth[2] = switchedDayMonth[2].TrimStart('2');
-        switchedDayMonth[2] = switchedDayMonth[2].TrimStart('0');
-        Console.WriteLine("" + Authors[i] + " @ " + switchedDayMonth[1] + "/" + switchedDayMonth[0] + "/" + switchedDayMonth[2] + ": " + Messages[i]);
+
+
+        for (int i = 0; i < Authors.Count; i++)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(Convert.ToDouble(Timestamps[i])).ToLocalTime();
+            string[] switchedDayMonth = dateTime.ToString().Split('/');
+            switchedDayMonth[2] = switchedDayMonth[2].TrimStart('2');
+            switchedDayMonth[2] = switchedDayMonth[2].TrimStart('0');
+            Console.WriteLine("" + Authors[i] + " @ " + switchedDayMonth[1] + "/" + switchedDayMonth[0] + "/" + switchedDayMonth[2] + ": " + Messages[i]);
+        }
     }
-}
-else if (args[0].ToLower() == "cheep")
-{
-    string csv = "";
-    string line = "";
-    for (int i = 1; i < args.Length; i++)
+    else if (args[0].ToLower() == "cheep")
     {
-        line += args[i];
+        string csv = "";
+        string line = "";
+        for (int i = 1; i < args.Length; i++)
+        {
+            line += args[i];
+        }
+        Cheep cheep = new(Environment.UserName, line, DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+        csv += cheep.Author + ",\"" + cheep.Message + "\"," + cheep.TimeStamp + Environment.NewLine;
+        File.AppendAllText(path, csv);
     }
-    
-    csv += Environment.UserName + ",\"" + line + "\"," + DateTimeOffset.UtcNow.ToUnixTimeSeconds() + Environment.NewLine;
-    File.AppendAllText(path, csv);
-}
+
+
+public record Cheep(string Author, string Message, long TimeStamp);
