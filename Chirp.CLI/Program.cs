f9a86da40cad92bd41539fabce242e5a04db4e38
Kian <kiaf@itu.dk>
2025-09-12 10:22:09 +0200

    User Interface class/method. Co-Authors: Patrick, pafr@itu.dk Oskar, oskk@itu.dk Gustav, gnil@itu.dk

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -2,7 +2,6 @@
 using System;
 using SimpleDB;
 
-
     public record Cheep(string Author, string Message, long Timestamp);
 
     class Program
@@ -14,20 +13,7 @@ using SimpleDB;
 
             if (args[0].ToLower() == "read")
             {
-                List<Cheep> cheeps = database.Read().ToList();
-
-
-
-                foreach (Cheep cheep in cheeps)
-                {
-                    DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-                    dateTime = dateTime.AddSeconds(Convert.ToDouble(cheep.Timestamp)).ToLocalTime();
-                    string[] switchedDayMonth = dateTime.ToString().Split('/');
-                    switchedDayMonth[2] = switchedDayMonth[2].TrimStart('2');
-                    switchedDayMonth[2] = switchedDayMonth[2].TrimStart('0');
-                    var time = switchedDayMonth[1] + "/" + switchedDayMonth[0] + "/" + switchedDayMonth[2];
-                    Console.WriteLine(cheep.Author + " @ " + time + ": " + cheep.Message);
-                }
+            UserInterface.PrintCheeps(database.Read().ToList());
             }
             else if (args[0].ToLower() == "cheep")
             {
