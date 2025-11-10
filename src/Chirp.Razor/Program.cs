7748bab616679092a6ff40d60f731ea36ccb624c
Gustav <gnil@itu.dk>
2025-11-10 15:16:22 +0100

    Temp fix

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -6,20 +6,20 @@ using Chirp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
+
+// Add services to the container.
+builder.Services.AddRazorPages();  // ← ONLY ONCE
+
 builder.Services.AddScoped<CheepService>();
 builder.Services.AddScoped<IMessageRepository, MessageRepository>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
-
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<CheepDbContext>(options => options.UseSqlite(connectionString));
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-
 var app = builder.Build();
+
+// Seed database
 using var scope = app.Services.CreateScope();
 var db = scope.ServiceProvider.GetRequiredService<CheepDbContext>();
 await DbInitializer.SeedDatabaseAsync(db);
@@ -27,21 +27,18 @@ await DbInitializer.SeedDatabaseAsync(db);
 var service = scope.ServiceProvider.GetRequiredService<CheepService>();
 await service.TestSeedAsync();
 
-// Configure the HTTP request pipeline.
+// Pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapRazorPages();  // ← this is correct
 
 app.Run();
